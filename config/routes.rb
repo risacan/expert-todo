7be989c071a4aacda81e61b996d44aa599f0dfdd
Risa Harada <supikateriburu@gmail.com>
2018-03-19 01:21:52 +0900

    :sparkles: Add login / logout with views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'pages/index'
+  root "pages#index"
   devise_for :users
+  devise_scope :user do
+    get "sign_in", to: "users/sessions#new"
+    get "sign_out", to: "users/sessions#destroy"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
